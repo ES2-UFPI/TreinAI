36a2b4c556e0a2306c549f1cd3c321c4cc055e90
Thalysson <thalyssonmelo840@gmail.com>
2026-06-01 10:49:03 -0300

    feat(db): modelo da entidade de User e migration dela

--- backend/domain/__init__.py
+++ backend/domain/__init__.py
@@ -2,5 +2,6 @@ from .workout import Workout, DifficultyLevel
 from .embedding import EmbeddingDocument
 from .exercise import Exercise
 from .workout_exercise import WorkoutExercise
+from .user import User
 
-__all__ = ["Workout", "DifficultyLevel", "EmbeddingDocument", "WorkoutExercise", "Exercise"]
+__all__ = ["Workout", "DifficultyLevel", "EmbeddingDocument", "WorkoutExercise", "Exercise", "User"]
