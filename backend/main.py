c7f8ccc99c513f0bfec8af9ca6567288bf229cbb
Artur Vinicius Silva Sousa <a.vinicius@tjpi.jus.br>
2026-06-29 10:30:27 -0300

    feat: corrigido estrutura do endpoint /help; closes #73

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from controllers.auth_controller import router as auth_router
+from controllers.help_controller import router as help_router
 from controllers.user_controller import router as user_router
 from controllers.workout_controller import router as workout_router
 
@@ -16,6 +17,7 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
+app.include_router(help_router)
 app.include_router(user_router)
 app.include_router(workout_router)
 
